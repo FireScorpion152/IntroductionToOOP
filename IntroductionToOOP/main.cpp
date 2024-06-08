a40fdcb506a084a0ec8043247d9561b4ae9ddd1d
yumin <yumin.dima@gmail.com>
2024-06-08 21:17:25 +0300

    CopyAssignment

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,9 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
-
+using std::cin;
+using std::cout;
+using std::endl;
+#define delimiter "\n--------------------------------------\n"
 class Point
 {
 	double x;
@@ -35,17 +38,33 @@ class Point
 		this->y = y;
 		cout << "2ArgConstructor:\t" << this << endl;
 	}
+
+	Point(const Point& other) {
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+
 	~Point() 
 	{
 		cout << "Destructor: \t\t" << this << endl;
 	}
 
+	//	Operators;
+	Point& operator=(const Point& other) {
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
 	// Methods:
 
-	double distance(Point other) {
+	double distance(Point& other)const {
 		double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		//this->x *= 10;
 		return distance;
 	}
 
@@ -54,18 +73,23 @@ class Point
 	}
 };
 
-double Distance(const Point& A, const Point& B) {
+double distance(const Point& A, const Point& B)
+{
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
-	return sqrt(x_distance * x_distance + y_distance * y_distance);
+	double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+	return distance;
 }
-
+//#define CONSTRUCTORS_CHECK
+//#define DISTANCE_CHECK
+#define ASSIGNMENT_CHECK
 void main() {
 	setlocale(LC_ALL, "");
 
+#ifdef CONSTRUCTORS_CHECK
 	Point A;
-	//A.set_x(2);
-	//A.set_y(3);
+	A.set_x(2);
+	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
 	A.print();
 
@@ -73,7 +97,44 @@ void main() {
 	B.print();
 	Point C(2, 3); //2ArgumentConstructor
 	C.print();
-	cout << A.distance(C) << endl;
-	cout << Distance(B, C) << endl;
+	Point D = C;
+	D.print();
+	
+	Point E;
+	E = D;
+	E.print();
+#endif // CONSTRUCTORS_CHECK
+#ifdef DISTANCE_CHECK
+
+	Point A(2, 3);
+	Point B(7, 8);
+
+	A.print();
+	B.print();
+
+	cout << delimiter << endl;
+	cout << "Расстояние от точки 'A' до точки 'B':" <<  A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки 'B' до точки 'A':" << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками 'A' до точки 'B':" << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками 'B' до точки 'A':" << distance(B, A) << endl;
+	cout << delimiter << endl;
+#endif // DISTANCE_CHECK
+#ifdef ASSIGNMENT_CHECK
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point(2, 3);		//Point(2,3) - явно вызываем конструктор\
+									и создаём временный безымянный объект,\
+									этот объект и передаётся как параметр\
+									в первый CopyAssignment.
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
+
 }
 
